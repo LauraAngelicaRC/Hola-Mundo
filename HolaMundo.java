da3c15cfdd200f4126feff597b482aa6c6f23238
Laura <18030089@itcelaya.edu.mx>
2020-10-18 15:13:48 -0500

    Mi primer commit

---
+++ HolaMundo.java
@@ -0,0 +1,21 @@
+
+package holamundo;
+
+
+public class HolaMundo {
+
+    public static void holamundo()
+    {
+        for( int i=1; i <=10; i++) //Ordena la matriz X
+        {      
+            for( int j=1;j<=10; j++)
+            {
+                System.out.println("Hola Mundo \n");
+            }
+        }
+    }
+    public static void main(String[] args) {
+       holamundo();
+    }
+    
+}
