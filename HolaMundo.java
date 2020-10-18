89d09116680056475d9387ac38c52248f7019d8c
Laura <18030089@itcelaya.edu.mx>
2020-10-18 15:20:25 -0500

    Es mi segundo commit

--- HolaMundo.java
+++ HolaMundo.java
@@ -6,9 +6,9 @@ public class HolaMundo {
 
     public static void holamundo()
     {
-        for( int i=1; i <=10; i++) //Ordena la matriz X
+        for( int i=1; i <=11; i++) //Ordena la matriz X
         {      
-            for( int j=1;j<=10; j++)
+            for( int j=1;j<=11; j++)
             {
                 System.out.println("Hola Mundo \n");
             }
